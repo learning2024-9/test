aa23f29bb6cb3580a0d46952607e0a169634072f
learning2024-9 <15269196660@163.com>
2024-09-13 00:50:13 +0800

    struct

--- test.cpp
+++ test.cpp
@@ -1,1 +1,33 @@
-//hello
+#include <iostream>
+using namespace std;
+typedef struct student{
+    string name;
+    int age;
+    int id;
+}student;
+int main(void)
+{
+    student student1,student2;
+    string name;
+    int age;
+    cout << "name1" <<endl;
+    cin >> name ;    
+    student1.name=name;
+
+
+    cout << "age1" <<endl;
+    // cin >>age;
+    student1.age=88;
+    // cout << "age2" <<endl;
+    // cin >>student2.age>>endl;
+    cout << "student1 " <<student1.name<<" "<<student1.age<<endl;
+    // cout << "student2" <<student2.name<<" "<<student2.age<<endl;
+
+    int arr[]={4,23,455,144};
+    int *p;
+    p=arr;
+    cout << p << endl;
+
+    cout << *++p << endl;
+    return 0;
+}
